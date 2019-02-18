c3b735d18bed3f60b07603fcce8648e02f941658
MariusMeiners <mariusm1175@gmail.com>
2019-02-18 07:29:41 +0100

    hovereffects

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -7,13 +7,13 @@ const SearchComponent = props => {
   return (
     <div className="searchComponent__container">
       <h1 className="searchComponent__title">Enter a word you have in mind</h1>
-      <h2 className="searchComponent_description">
+      <h2 className="searchComponent__description">
         Have you ever felt stuck rapping 🎤 or writing that poem ✍️ to impress
         your crush?
         <br />
         <span> Struggle no longer.</span>
       </h2>
-      <form className="searchComponent_form">
+      <form className="searchComponent__form">
         <input
           type="text"
           value={props.rhymeWord}
@@ -37,11 +37,13 @@ const SearchComponent = props => {
           ))}
         </div>
       </form>
-      <p>
-        <span>HINT: </span>you can also enter names to create cool references to
-        your favorite movies or brands.
+      <p className="SearchComponent__hint">
+        <span className="SearchComponent__hinttext">HINT: </span>
+        <br />
+        you can also enter names to create cool references to your favorite
+        movies or brands.
       </p>
-      <button className="SearchComponent_ratebutton">RATE</button>
+      <button className="SearchComponent__ratebutton">RATE</button>
     </div>
   );
 };
